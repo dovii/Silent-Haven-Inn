2b653a3d4abbf41e0abc47927062b129284f1834
Dovile <43847677+dovii@users.noreply.github.com>
2021-06-03 14:57:55 +0200

    reupload

---
+++ Library/Collab/Original/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public LayerMask solidObjectsLayer;
+    public LayerMask interactableLayer;
+
+    private bool isMoving = false;
+    private Vector2 input;
+    
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal"); //move with A/D or left/right arrow keys
+            input.y = Input.GetAxisRaw("Vertical"); //move with W/S or up/down arrow keys
+
+            if (input.x != 0) input.y = 0; //restrict movement diagonally
+
+            if (input != Vector2.zero)
+            {
+                var movement = transform.position;
+                movement.x += input.x;
+                movement.y += input.y;
+
+                if (isPassable(movement)){
+                    StartCoroutine(Move(movement));
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+            Interact();
+    }
+
+
+    void Interact()
+    {
+        var facingDir = new Vector3(input.x, input.y);
+        //change to this after animations are done:
+        //var facingDir = new Vector3 (animator.GetFloat("moveX"), animator.GetFloat("moveY"));
+
+        var interactPos = transform.position + facingDir; //position of the tile to which the player is facing
+
+        //Debug.DrawLine(transform.position, interactPos, Color.red, 2f);
+
+        var colliderNPC = Physics2D.OverlapCircle(interactPos, 0.5f, interactableLayer);
+        
+        if (colliderNPC != null)
+        {
+            colliderNPC.GetComponent<Interactable>()?.Interact();
+        }
+    }
+
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+        isMoving = false;
+    }
+
+    private bool isPassable(Vector3 targetPos)
+    {
+        if(Physics2D.OverlapCircle(targetPos, 0.3f, solidObjectsLayer | interactableLayer) != null)
+        {
+            return false;
+        }
+        return true;
+    }
+
+}
